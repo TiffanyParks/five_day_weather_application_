1c3c8137f579d4bad7e71e47fb4ff5028286c082
Tiffany <tapintobeauty08@gmail.com>
2023-04-30 16:40:28 -0400

    update stylesheet to reflect model

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,6 @@
-// var inputDisplay= $('#inputDisplay');
+var submitBtn = document.getElementById('submitBtn')
 
-// //var titleEl = document.createElement("h2");
-// var titleEl = $('<h3>');
-
-// //titeEl.textContent = "Weather Dashboard";
-// titleEl.text('Weather Dashboard');
-
-// //titleEl.className = 'mainDisplay';
-// titleEl.attr('class', 'mainDisplay');
-
-// //title.El.classList.add('p-2')m- ('p-' is for padding);
-// titleEl.addClass('p-5');
-
-// //title.style.background = "rgb(0, 0, 255)";
-// titleEl.css('background-color', 'rgb(0, 0, 255)');
-// titleEl.css('color', 'rgb(255, 255, 255)');
-// titleEl.scrollLeft('2px');
-// //inputText.appendChild(titleEl);
-// inputDisplay.append(titleEl);
-
-// //titleEl.append('Weather Dashboard');
-// inputDisplay.append('<h5 id="inputEl"> Search for a City:</h5>');
-
-// var inputDisplay = $('#inputDisplay');
-// var cityInputVaule = $('<input>');
-
-// cityInputVaule.attr('class', 'city-input');
-// cityInputVaule.attr('tpye', 'test');
-// cityInputVaule.attr('playholder', 'Enter a city');
-
-// cityInputVaule.height("4vh");
-// cityInputVaule.css('color', 'rgb(100, 149, 237)');
-// // cityInputValue.css('border', 'none'),ls
-
-// // btnEl.c
-
-
-var searchBtn = document.getElementById('searchBtn')
-
-var inputEl = document.getElementById('cityInput')
+var cityInput = document.getElementById('city-input')
 var apiKey = '0639c54aa963cab94c1a9a968f720718'
 function getCoord(cityName){
     var getoUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityName + '&appid=' + apiKey
@@ -50,10 +12,12 @@ function getCoord(cityName){
         console.log(data)
     })
 }
-searchBtn.addEventListener("click", function(){
-var inputVal = inputEl.value
-getCoord(inputVal)
+submitBtn.addEventListener("click", function(){
+var cityNameVal = cityInput.value
+getCoord(cityNameVal)
 })
+
+
 
 
 
